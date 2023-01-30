ffc1c5b62ccc19cb63fa75f54afa1c0f40d90d00
Nick(Seungyun Kim) <sy5072@naver.com>
2023-01-31 01:19:49 +0900

    nullable

--- main.dart
+++ main.dart
@@ -1,12 +1,8 @@
 void main() {
-    dynamic name;
-    name = "Niku";
-    if(name is String) {
-        print(name.length);
-    }
-    name = 12;
-    if (name is int) {
-      print(name.isEven);
-    }
-    name = true;
+    String? nick = "nick";
+    nick = null;
+    // if (nick != null) {
+    //   nick.isNotEmpty;
+    // }
+    nick?.isNotEmpty;
 }
