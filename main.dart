c46b0309edd87d00eab23d93fe03fb248d0d4ad4
Nick(Seungyun Kim) <sy5072@naver.com>
2023-02-02 17:22:12 +0900

    Maps

--- main.dart
+++ main.dart
@@ -1,11 +1,17 @@
 void main() {
-  var oldFriends = ['nick', 'sdaq'];
-  var newFriends = [
-    'taek',
-    'joy',
-    'murphy',
-    for (var friend in oldFriends) "@$friend",
-  ];
+  var player = {
+    'name': 'nick',
+    'xp': 19.99,
+    'superpower': false,
+  };
 
-  print(newFriends);
+  Map<int, bool> player2 = {
+    1: true,
+    2: false,
+    3: true,
+  };
+
+  Map<List<int>, bool> player3 = {
+    [1, 2, 3]: true,
+  };
 }
