79354688ceec309a81d48e921d668df968c21d8f
Nick(Seungyun Kim) <sy5072@naver.com>
2023-02-02 17:24:55 +0900

    Set

--- main.dart
+++ main.dart
@@ -1,17 +1,8 @@
 void main() {
-  var player = {
-    'name': 'nick',
-    'xp': 19.99,
-    'superpower': false,
-  };
-
-  Map<int, bool> player2 = {
-    1: true,
-    2: false,
-    3: true,
-  };
-
-  Map<List<int>, bool> player3 = {
-    [1, 2, 3]: true,
-  };
+  var numbers = {1, 2, 3, 4};
+  Set<int> numbers2 = {1, 2, 3, 4};
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  print(numbers);
 }
