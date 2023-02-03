d730edf9e47b9dea59dcae1a009ce95157e83123
Nick(Seungyun Kim) <sy5072@naver.com>
2023-02-03 10:54:27 +0900

    typedef

--- main.dart
+++ main.dart
@@ -1,13 +1,16 @@
-String capitalizeName(String? name) => name?.toUpperCase() ?? "ANON";
+typedef ListOfInts = List<int>;
+typedef UserInfo = Map<String, String>;
+
+ListOfInts reverseListOfNumbers(ListOfInts list) {
+  var reversed = list.reversed;
+  return reversed.toList();
+}
+
+String sayHi(UserInfo userInfo) {
+  return "Hi ${userInfo['name']}";
+}
 
 void main() {
-  capitalizeName('nick');
-  capitalizeName(null);
-
-  String? name;
-  name ??= 'nick';
-  print(name);
-  name = null;
-  name ??= "another";
-  print(name);
+  reverseListOfNumbers([1, 2, 3]);
+  print(sayHi({"name": "Nick"}));
 }
