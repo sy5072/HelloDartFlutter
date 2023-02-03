6a422f1655cd452d93debdb9fcbab1d2b6c3ba85
Nick(Seungyun Kim) <sy5072@naver.com>
2023-02-03 11:28:33 +0900

    named constructors

--- main.dart
+++ main.dart
@@ -10,24 +10,30 @@ class Player {
       required this.team,
       required this.age});
 
+  Player.createBluePlayer({required String name, required int age})
+      : this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  Player.createRedPlayer(String name, int age)
+      : this.age = age,
+        this.name = name,
+        this.team = 'red',
+        this.xp = 0;
+
   void sayHello() {
     print("Hi my name is $name");
   }
 }
 
 void main() {
-  var player = Player(
-    xp: 1500,
+  var player = Player.createBluePlayer(
     name: "nick",
-    team: "SF",
     age: 25,
   );
-  var player2 = Player(
-    name: "dake",
-    xp: 800,
-    team: "Blue",
-    age: 28,
+  var player2 = Player.createRedPlayer(
+    "dake",
+    28,
   );
-  player.sayHello();
-  player2.sayHello();
 }
