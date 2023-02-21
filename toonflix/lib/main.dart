80db3b7d6bf9f00b8a377f645b6c37c60298d971
Nick(Seungyun Kim) <sy5072@naver.com>
2023-02-21 20:47:33 +0900

    toonflix/header

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,14 +9,42 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Hello Flutter!"),
-          ),
-        body: Center(
-          child: Text("Hello World!"),
+        backgroundColor: Color(0xff181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Hey, Selena",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        "Welcome back",
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.7),
+                          fontSize: 14,),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
           ),
         ),
+      ),
     );
   }
 }
