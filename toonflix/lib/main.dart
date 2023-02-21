27d6a311081860e7ab1cd723ff7968870d6d1c1c
Nick(Seungyun Kim) <sy5072@naver.com>
2023-02-21 19:45:04 +0900

    Flutter Widget

---
+++ toonflix/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Hello Flutter!"),
+          ),
+        body: Center(
+          child: Text("Hello World!"),
+          ),
+        ),
+    );
+  }
+}
