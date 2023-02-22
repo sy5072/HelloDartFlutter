58c063d4f056b20a140ee8271ee48d55bd30a207
Nick(Seungyun Kim) <sy5072@naver.com>
2023-02-22 19:14:02 +0900

    toonflix/Buttons Section

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -13,6 +13,7 @@ class App extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -35,12 +36,57 @@ class App extends StatelessWidget {
                         "Welcome back",
                         style: TextStyle(
                           color: Colors.white.withOpacity(0.7),
-                          fontSize: 14,),
+                          fontSize: 14,
+                        ),
                       ),
                     ],
                   )
                 ],
               ),
+              SizedBox(
+                height: 100,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white.withOpacity(0.6),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 42,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                      child: Text(
+                        "Transfer",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
