01d8f9bb04fa68f8922cb00c8c80b8cb799930e2
Nick(Seungyun Kim) <sy5072@naver.com>
2023-03-03 18:30:22 +0900

    pomodoro/timer

--- pomodoro/lib/screens/home_screen.dart
+++ pomodoro/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds -= 1;
+    });
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                "25:00",
+                "$totalSeconds",
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 89,
@@ -34,7 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: IconButton(
               iconSize: 110,
               color: Theme.of(context).cardColor,
-              onPressed: () {},
+              onPressed: onStartPressed,
               icon: const Icon(Icons.play_circle_outline_outlined),
             )),
           ),
@@ -45,8 +60,9 @@ class _HomeScreenState extends State<HomeScreen> {
                 Expanded(
                   child: Container(
                     decoration: BoxDecoration(
-                      color: Theme.of(context).cardColor,
-                    ),
+                        color: Theme.of(context).cardColor,
+                        borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(50))),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
