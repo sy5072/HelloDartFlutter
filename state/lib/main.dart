c1dd68e5666b7440f6c6b4b66c1b1338bb8b6087
Nick(Seungyun Kim) <sy5072@naver.com>
2023-03-03 16:47:54 +0900

    state/recap

--- state/lib/main.dart
+++ state/lib/main.dart
@@ -12,11 +12,11 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
+  List<int> numbers = [];
 
   void onClicked() {
     setState(() {
-      counter += 1;
+      numbers.add(numbers.length);
     });
   }
 
@@ -26,26 +26,24 @@ class _AppState extends State<App> {
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Click Count",
-              style: TextStyle(
-                fontSize: 30,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Click Count",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
               ),
-            ),
-            Text(
-              '$counter',
-              style: const TextStyle(fontSize: 30),
-            ),
-            IconButton(
-              iconSize: 40,
-              onPressed: onClicked,
-              icon: const Icon(Icons.add_box_rounded),
-            ),
-          ],
-        )),
+              for (var n in numbers) Text('$n'),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
